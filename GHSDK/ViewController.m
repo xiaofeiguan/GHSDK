8a03df9b56606b59f934349766a27a35fcc485f1
小肥观 <liuguanhua@localhost>
2016-09-13 18:01:45 +0800

    Initial Commit

---
+++ GHSDK/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GHSDK
+//
+//  Created by 刘观华 on 16/9/13.
+//  Copyright © 2016年 SmoothV. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
